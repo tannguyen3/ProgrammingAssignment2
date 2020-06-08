26dae1665f369c710a522f80db971fcb063be02e
Tan Nguyen <tan.nguyen2@halliburton.com>
2020-06-08 16:10:26 -0500

    complete the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment file for Week 3 of R Programming Cousera course.
+## Use below code to test the functions
+# A <- matrix( c(5, 1, 0,
+#                3,-1, 2,
+#                4, 0,-1), nrow=3, byrow=TRUE)
+# B <- matrix(rnorm(1:20), nrow=4, byrow = TRUE)
+# cacheMatrix <- makeCacheMatrix(A)
+# cacheMatrix$inverse() #first time call so the inverse will be calculated
+# cacheMatrix$inverse() #second time call so will have the result from the cache - inverseX
+# cacheMatrix <- cacheMatrix$set(B) # set new value for the matrix 
+# cacheMatrix$inverse() #first time call after set the new so the inverse will be calculated
+# cacheMatrix$inverse() #second time call so will have the result from the cache - inverseX
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+  inverseX <- NULL
+  
+  get <- function() x
+  set <- function(y){
+    x <<- y
+    inverseX <<- NULL
+  }
 
+  inverse <- function() {
+    if (is.null(inverseX)) {
+      inverseX <<- solve(x)
+    }
+    inverseX
+  }
+  
+  list(get = get, set = set, inverse = inverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
